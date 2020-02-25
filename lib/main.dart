3b0fcdaafb8bfd066eed1faa18fb2f3c05bbf6b9
Ayush783 <ayushc45xxx@gmail.com>
2020-02-25 18:03:20 +0530

    post widget added and buttons made responsive

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,15 @@ class _MyAppState extends State<MyApp> {
   //appbar
   Widget _appbar() {
     return AppBar(
+      titleSpacing: .2,
       leading: RawMaterialButton(
+        splashColor: Colors.white,
         onPressed: () {
           print("camera pressed");
         },
         child: Icon(
           MaterialCommunityIcons.camera_outline,
-          size: 28,
+          size: 30,
         ),
       ),
       title: Text(
@@ -30,8 +32,8 @@ class _MyAppState extends State<MyApp> {
         Padding(
           padding: EdgeInsets.only(right: 0),
           child: RawMaterialButton(
-            constraints: BoxConstraints(maxHeight: 30,minWidth: 30),
-            padding: EdgeInsets.all(0),
+            constraints: BoxConstraints(maxHeight: 30, minWidth: 30),
+            splashColor: Colors.white,
             onPressed: () {
               print("chat icon pressed");
             },
@@ -130,6 +132,7 @@ class _MyAppState extends State<MyApp> {
         child: Scaffold(
           appBar: _appbar(),
           body: ListView(
+            physics: BouncingScrollPhysics(),
             scrollDirection: Axis.vertical,
             children: <Widget>[
               Container(
@@ -142,43 +145,25 @@ class _MyAppState extends State<MyApp> {
                 padding: EdgeInsets.symmetric(horizontal: 10),
                 child: storybar(),
               ),
-              Container(
-                height: 55,
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(color: Colors.black12),
-                  ),
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Color(0xffdc3171),
-                    radius: 20,
-                    child: CircleAvatar(
-                      radius: 18,
-                      backgroundImage: AssetImage('images/dsc.png'),
-                    ),
-                  ),
-                  title: Text(
-                    "Dsc Kiet",
-                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
-                  ),
-                  trailing: Icon(
-                    Icons.more_vert,
-                    color: Colors.black,
-                  ),
-                ),
+              Post(
+                accName: "Dsc Kiet",
+                img:
+                  'https://cloud.google.com/images/cloud-startups-program/cloud_for_startups_hero_image_1x.png',
+                caption:
+                    "Hello Guys!! this is official insta handle of Google developer students club",
+                likenum: 2658,
+                commentNum: 64,
+                accImg:
+                    'https://media-exp1.licdn.com/dms/image/C560BAQHtS3OdZ0Kr8Q/company-logo_200_200/0?e=2159024400&v=beta&t=NFD7U4LerS47Ui4c03fBDH_6BsG9vTQm081EVKBxYc0',
               ),
-              Column(
-                children: <Widget>[
-                  Image.asset('images/dscp.png'),
-                  ListTile(
-                    leading: Icon(
-                      MaterialCommunityIcons.heart_outline,
-                      color: Colors.black,
-                      size: 30,
-                    ),
-                  ),
-                ],
+              Post(
+                accName: "Rober Downey Jr.",
+                img:
+                    'https://www.hindustantimes.com/rf/image_size_640x362/HT/p1/2015/04/03/Incoming/Pictures/1333507_Wallpaper2.jpg',
+                likenum: 623459,
+                commentNum: 989,
+                caption: "Good morning guys",
+                accImg: "https://www.californiamuseum.org/sites/main/files/imagecache/lightbox/main-images/robertdowneyjr_cahalloffameinductee.png",
               )
             ],
           ),
@@ -188,14 +173,7 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-// Container(
-//                     height: 300,
-//                     decoration: BoxDecoration(
-//                       border: Border(
-//                         bottom: BorderSide(color: Colors.black12),
-//                       ),
-//                     ),
-//                   ),
+//class storybar :
 
 class Storybar {
   String text;
@@ -207,12 +185,20 @@ class Storybar {
         Padding(
           padding: EdgeInsets.only(top: 6),
         ),
-        CircleAvatar(
-          backgroundColor: Color(0xffdc3171),
-          radius: 30,
+        RawMaterialButton(
+          constraints: BoxConstraints(minHeight: 30),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          onPressed: () {
+            print("story button pressed");
+          },
           child: CircleAvatar(
-            radius: 28,
-            backgroundImage: NetworkImage(url),
+            backgroundColor: Color(0xffdc3171),
+            radius: 30,
+            child: CircleAvatar(
+              radius: 28,
+              backgroundImage: NetworkImage(url),
+            ),
           ),
         ),
         Padding(
@@ -221,7 +207,178 @@ class Storybar {
         Text(
           text,
           style: TextStyle(fontSize: 11, fontWeight: FontWeight.w400),
-        )
+        ),
+      ],
+    );
+  }
+}
+
+//post class
+
+class Post extends StatefulWidget {
+  final String img;
+  final int likenum;
+  final String caption;
+  final String accName;
+  final int commentNum;
+  final String accImg;
+  Post(
+      {Key key,
+      this.img,
+      this.accName,
+      this.caption,
+      this.commentNum,
+      this.accImg,
+      this.likenum});
+  @override
+  _PostState createState() => _PostState();
+}
+
+class _PostState extends State<Post> {
+  @override
+  Widget build(BuildContext context) {
+    int likenum = widget.likenum;
+    int commentNum = widget.commentNum;
+    Color color = Colors.white;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          height: 55,
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(color: Colors.black12),
+            ),
+          ),
+          child: ListTile(
+            leading: RawMaterialButton(
+              splashColor: Colors.white,
+              constraints: BoxConstraints(minHeight: 30),
+              onPressed: () {
+                print("pressed");
+              },
+              child: CircleAvatar(
+                backgroundColor: Color(0xffdc3171),
+                radius: 20,
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundImage: NetworkImage(widget.accImg),
+                ),
+              ),
+            ),
+            contentPadding: EdgeInsets.fromLTRB(8, 0, 10, 0),
+            title: Text(
+              widget.accName,
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
+            ),
+            trailing: Icon(
+              Icons.more_vert,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Image.network(widget.img),
+        Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 3),
+            ),
+            RawMaterialButton(
+                constraints: BoxConstraints(minHeight: 30, minWidth: 30),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                onPressed: () {
+                  setState(() {
+                    color = Colors.red;
+                  });
+                  print("like button got pressed");
+                },
+                child: Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: <Widget>[
+                    Icon(
+                      MaterialCommunityIcons.heart,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                    Icon(
+                      MaterialCommunityIcons.heart,
+                      color: color,
+                      size: 27,
+                    )
+                  ],
+                )),
+            RawMaterialButton(
+                constraints: BoxConstraints(minHeight: 20, minWidth: 30),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                onPressed: () {
+                  print("comment button got pressed");
+                },
+                child: Image.asset('images/cmnt.png', height: 28)),
+            RawMaterialButton(
+                constraints: BoxConstraints(minHeight: 20, minWidth: 30),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                onPressed: () {
+                  print("dm got pressed");
+                },
+                child: Image.asset('images/dm.png', height: 24)),
+            Padding(
+              padding: EdgeInsets.only(right: 155),
+            ),
+            RawMaterialButton(
+                constraints: BoxConstraints(minHeight: 20, minWidth: 30),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                onPressed: () {
+                  print("save got pressed");
+                },
+                child: Image.asset('images/save.png', height: 24)),
+            Padding(
+              padding: EdgeInsets.only(right: 10),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "$likenum Likes",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+              ),
+              Text.rich(
+                TextSpan(
+                  text: widget.accName + " ",
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: widget.caption,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+              ),
+              Text(
+                "View all $commentNum comments",
+                style: TextStyle(color: Colors.grey),
+              ),
+              Padding(padding: EdgeInsets.only(bottom:10),)
+            ],
+          ),
+        ),
       ],
     );
   }
