6f0cba559cfb4d6fc7bb3bd5c218eb35c10977cb
Ayush783 <ayushc45xxx@gmail.com>
2021-05-10 20:55:40 +0530

    dashboard created

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'camera_screen.dart';
+import 'homepage.dart';
+import 'inbox_screen.dart';
 
-List<Widget> dashBoardScreens = [];
+List<Widget> dashBoardScreens = [
+  CameraScreen(),
+  Homepage(),
+  InboxScreen(),
+];
 
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PageView();
+    return PageView(
+      children: dashBoardScreens,
+    );
   }
 }
