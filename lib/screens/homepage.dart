8f37d2c9c7f86da1215b835e1f71c64a34e3f680
Ayush783 <ayushc45xxx@gmail.com>
2021-05-10 21:24:22 +0530

    app bar moved to widgets file

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:supercharged/supercharged.dart';
+import 'package:insta_clone/widgets/homepage_app_bar.dart';
 
 class Homepage extends StatelessWidget {
   final PageController controller;
@@ -9,46 +8,7 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomepageAppBar(controller: controller),
-    );
-  }
-}
-
-class HomepageAppBar extends StatelessWidget {
-  const HomepageAppBar({
-    Key key,
-    @required this.controller,
-  }) : super(key: key);
-
-  final PageController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      leading: IconButton(
-          icon: SvgPicture.asset(
-            'assets/icons/plus.svg',
-            height: 24,
-          ),
-          onPressed: () {
-            controller.animateToPage(0,
-                duration: 300.milliseconds, curve: Curves.easeInExpo);
-          }),
-      title: Text(
-        "Instagram",
-        style: TextStyle(fontFamily: 'Billa', fontSize: 30),
-      ),
-      actions: [
-        IconButton(
-            icon: SvgPicture.asset(
-              'assets/icons/dm.svg',
-              height: 24,
-            ),
-            onPressed: () {
-              controller.animateToPage(2,
-                  duration: 300.milliseconds, curve: Curves.easeInExpo);
-            }),
-      ],
+      appBar: HomepageAppBar(controller: controller),
     );
   }
 }
---
+++ lib/widgets/homepage_app_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:supercharged/supercharged.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomepageAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomepageAppBar({
+    Key key,
+    @required this.controller,
+  }) : super(key: key);
+
+  final PageController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+          icon: SvgPicture.asset(
+            'assets/icons/plus.svg',
+            height: 24,
+          ),
+          onPressed: () {
+            controller.animateToPage(0,
+                duration: 300.milliseconds, curve: Curves.easeInExpo);
+          }),
+      title: Text(
+        "Instagram",
+        style: TextStyle(fontFamily: 'Billa', fontSize: 30),
+      ),
+      actions: [
+        IconButton(
+            icon: SvgPicture.asset(
+              'assets/icons/dm.svg',
+              height: 24,
+            ),
+            onPressed: () {
+              controller.animateToPage(2,
+                  duration: 300.milliseconds, curve: Curves.easeInExpo);
+            }),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size(double.maxFinite, double.maxFinite);
+}
