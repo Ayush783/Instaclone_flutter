c4d32dd61a42e73c446385962c6a5370fa817b30
Ayush783 <ayushc45xxx@gmail.com>
2021-05-15 23:29:57 +0530

    search screen built

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -23,60 +23,62 @@ class _DashboardState extends State<Dashboard> {
   int _index = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: 0,
-        elevation: 0,
-        iconSize: 24,
-        onTap: (value) {
-          _index = value;
-          setState(() {});
-        },
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: [
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                'assets/icons/home.svg',
-                height: 24,
-              ),
-              label: ''),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                'assets/icons/search.svg',
-                height: 24,
-              ),
-              label: ''),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                'assets/icons/plus.svg',
-                height: 24,
-              ),
-              label: ''),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset(
-                'assets/icons/heart_outlined.svg',
-                height: 24,
-              ),
-              label: ''),
-          BottomNavigationBarItem(
-              icon: Container(
-                height: 24,
-                width: 24,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/images/me.jpg',
+    return SafeArea(
+      child: Scaffold(
+        body: screens[_index],
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: 0,
+          elevation: 0,
+          iconSize: 24,
+          onTap: (value) {
+            _index = value;
+            setState(() {});
+          },
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/home.svg',
+                  height: 24,
+                ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/search.svg',
+                  height: 24,
+                ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/plus.svg',
+                  height: 24,
+                ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'assets/icons/heart_outlined.svg',
+                  height: 24,
+                ),
+                label: ''),
+            BottomNavigationBarItem(
+                icon: Container(
+                  height: 24,
+                  width: 24,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/me.jpg',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              label: ''),
-        ],
+                label: ''),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,8 +1,165 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.3),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    'assets/icons/search.svg',
+                    height: 20,
+                  ),
+                  Padding(padding: EdgeInsets.only(right: 16)),
+                  Text(
+                    'Search',
+                    style: TextStyle(
+                      color: Colors.grey.withOpacity(0.8),
+                      fontSize: 16,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 270,
+            width: double.infinity,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: Colors.green,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 2)),
+                      Expanded(
+                        child: Container(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(right: 2)),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(bottom: 2)),
+          Container(
+            height: 270,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: Colors.green,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 2)),
+                      Expanded(
+                        child: Container(
+                          color: Colors.red,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 2)),
+                      Expanded(
+                        child: Container(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 2)),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: Colors.green,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 2)),
+                      Expanded(
+                        child: Container(
+                          color: Colors.red,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 2)),
+                      Expanded(
+                        child: Container(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(bottom: 2)),
+          Container(
+            height: 270,
+            width: double.infinity,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(right: 2)),
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: Colors.green,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 2)),
+                      Expanded(
+                        child: Container(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
